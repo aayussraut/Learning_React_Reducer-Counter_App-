66aefa1c714c106374d6c4f2feeb9db7760d2f8a
Aayush Raut <aayussraut.ar@gmail.com>
2023-02-25 20:58:27 +0545

    Using Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { useSelector, useDispatch } from 'react-redux'
+import { actions } from './store/index'
 
 function App() {
   //useSelector reads a value from the store state and 
@@ -11,14 +12,14 @@ function App() {
   //useDispatch returns the store's dispatch method, which is used to dispatch actions to the store
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch({ type: 'INCREMENT' }); //dispatches an action to the store 
+    dispatch(actions.increment()); //dispatches an action to the store 
   }
   const decrement = () => {
-    dispatch({ type: 'Decrement' }); //dispatches an action to the store 
+    dispatch(actions.decrement());
   }
 
   const AddBy = () => {
-    dispatch({ type: "AddBy", payload: 10 })
+    dispatch(actions.addby(10));
   }
 
 
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,33 +1,25 @@
-import { legacy_createStore as createStore } from "redux";
-/*
-    * reducerFn is a function that takes two arguments:
-    * 1. state : current state value
-    * 2. action: describes what happened
-    * 
-    * returns the new state
-*/
-const reducerFn = (state = { counter: 0 }, action) => {
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-    /*
-        *limitation
-        * 1. we can't have async code here (Must Be Synchronous Function)
-        * 2. we should not mutate the state
-        
-    */
-    if (action.type === 'INCREMENT') {
-        return { counter: state.counter + 1 }
+const counterSlice = createSlice({
+    name: "counter",
+    initialState: { counter: 0 },
+    reducers: {
+        increment(state, action) {
+            state.counter += 1;
+        },
+        decrement(state, action) {
+            state.counter -= 1;
+        },
+        addby(state, action) {
+            state.counter += action.payload;
+        }
     }
-    if (action.type === 'Decrement') {
-        return { counter: state.counter - 1 }
-    }
-    if (action.type === "AddBy") {
-        return { counter: state.counter + action.payload }
-    }
-    return state;
-}
+})
 
-/*
-    * create a redux store holding the state of our app
-*/
-const store = createStore(reducerFn);
+export const actions = counterSlice.actions;
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+
 export default store;
